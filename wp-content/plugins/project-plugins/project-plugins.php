93ae0e40ab2ae8b9ace44bce9274db85c1177338
tmti20 <tmti20@gmail.com>
2019-03-06 13:20:24 -0500

    Task: First plugins initial files upload

---
+++ wp-content/plugins/project-plugins/project-plugins.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Very First Plugin
+ * Plugin URI: https://www.yourwebsiteurl.com/
+ * Description: This is the very first plugin I ever created.
+ * Version: 1.0
+ * Author: Your Name Here
+ * Author URI: http://yourwebsiteurl.com/
+ **/
+?>
